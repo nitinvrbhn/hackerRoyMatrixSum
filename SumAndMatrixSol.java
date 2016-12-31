d48b9ce1d4143358167cd17d115dfc77f75f42b0
Nitin Chaudhary <Nitin Chaudhary>
2016-12-31 11:40:22 +0530

    Adding the comments

--- SumAndMatrixSol.java
+++ SumAndMatrixSol.java
@@ -1,19 +1,21 @@
 public class Matrix {
 
    public static void main(String[] args) {
-   int N = 2;
-   
+	// Input is not implemented   
+	int N = 2;
+	
 	long sum = GetSum(N);
-   // call floor and print the result
-   System.out.println(sum);
+	System.out.println(sum);
    }
+   // Function to get the sum of Matrix
    public static long GetSum(int N){
 	   long sum = 0;
 	   int divFloor, extraAddition;
+	   // Gets the sum column-wise
 	   for(int j = 1; j<= N; j++){
 		   divFloor = N/j;
            extraAddition = ((N+1) % j) != 0 ? (divFloor * (j - (N+1) % j )) : 0;
-		   sum += ((j * (divFloor * (divFloor + 1)) / 2) - extraAddition);
+		   sum += ((j * (divFloor * (divFloor + 1)) / 2) - extraAddition);							// Sum of values in jth column 
 	   }
 	   return sum;
    }
